2917feb474fe55501129ecb79ae6ee4be34b2d2a
vidyashree c v <69243283+vidyashreecv2492@users.noreply.github.com>
2021-05-12 11:28:09 +0530

    Angular task 1

---
+++ task1/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { FirstComponent } from './first/first/first.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FirstComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
